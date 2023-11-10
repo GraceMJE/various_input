e224fbe9c94eb73ee694dddf2f57a5763765e3b6
GraceMJE <akwndms123@naver.com>
2023-11-10 12:42:17 +0900

    checkbox

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: Body(),
       )
@@ -15,6 +15,46 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Column(
+      children: [
+        TestCheckBox(),
+
+      ],
+    );
+  }
+}
+
+// 사용자의 입력에 따라 출력 바뀌기에 stateful로!
+class TestCheckBox extends StatefulWidget {
+  const TestCheckBox({super.key});
+
+  @override
+  State<TestCheckBox> createState() => _TestCheckBoxState();
+}
+
+class _TestCheckBoxState extends State<TestCheckBox> {
+  late List<bool> values;
+
+  @override
+  void initState() {
+    super.initState();
+    values = [false, false, false];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(value: values[0], onChanged: (value) => changeValue(0, value: value)),
+        Checkbox(value: values[1], onChanged: (value) => changeValue(1, value: value)),
+        Checkbox(value: values[2], onChanged: (value) => changeValue(2, value: value)),
+      ]
+    );
+  }
+
+  void changeValue(int index, {bool? value = false}) {
+    setState(() {
+      values[index] = value!;
+    });
   }
 }

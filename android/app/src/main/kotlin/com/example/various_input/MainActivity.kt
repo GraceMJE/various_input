3df8e4c6a75b7fea90ef1b45cddb8f2ece3d646d
GraceMJE <akwndms123@naver.com>
2023-11-10 11:38:39 +0900

    다양한 input과정의 시작

---
+++ android/app/src/main/kotlin/com/example/various_input/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.various_input
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Body(),
+      )
+    )
+  );
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
